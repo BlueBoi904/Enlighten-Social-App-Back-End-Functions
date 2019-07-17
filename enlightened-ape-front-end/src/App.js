54491fcaab2b203bee8350bfabea1537b0279dba
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-17 16:09:27 -0400

    Setup basic routing and page layout

--- enlightened-ape-front-end/src/App.js
+++ enlightened-ape-front-end/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+// Pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
 
 function App() {
   return (
-    <div>
-      <h1>Our app</h1>
+    <div className="App">
+      <Router>
+      <Switch>
+        <Route exact path ='/' component={home}/>
+        <Route exact path ='/login' component={login}/>
+        <Route exact path ='/signup' component={signup}/>
+      </Switch>
+      </Router>
     </div>
   );
 }
---
+++ enlightened-ape-front-end/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>navbar</h1>
+            </div>
+        )
+    }
+}
+
+export default navbar;
