9fb0229da1426f8ad94bdc1df6c7e94e2fc88fa8
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-17 20:34:23 -0400

    Hit endpoint with get request, displaying whispers on homepage

--- enlightened-ape-front-end/src/pages/home.js
+++ enlightened-ape-front-end/src/pages/home.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
-
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
 class home extends Component {
+    state = {
+        whispers: null
+    }
+    /*
+    axios.get('https://a.4cdn.org/a/threads.json', {
+	headers: {
+	  'Access-Control-Allow-Origin': '*',
+	},
+	proxy: {
+	  host: '104.236.174.88',
+	  port: 3128
+    }
+    */
+
+    
+    componentDidMount(){
+        axios.get('/whispers')
+          .then(res => {
+              console.log(res.data);
+            this.setState({
+                whispers: res.data
+            })
+          })
+          .catch(err => console.log(err));
+    }
+    
     render() {
+        const {whispers} = this.state;
+
+        let recentWhispersMarkup = whispers ? (
+            whispers.map(whisper => <p> {whisper.body}</p>)
+        ) : <p>Loading...</p>
         return (
-            <div>
-                <h1>home</h1>
-            </div>
+            <Grid container spacing={10}>
+                <Grid item sm={8} xs={12}>
+                    {recentWhispersMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile...</p>
+                </Grid>
+            </Grid>
         )
     }
 }
